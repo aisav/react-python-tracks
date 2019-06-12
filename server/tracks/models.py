84d854062205be6f0536d72320630130bcd81c07
aisav <artur.isaverdyan@gmail.com>
2019-06-12 23:56:30 +0400

    tracks migrations

--- server/tracks/models.py
+++ server/tracks/models.py
@@ -3,4 +3,7 @@ from django.db import models
 # Create your models here.
 
 class Track(models.Model):
-    title = models.Char
+    title = models.CharField(max_length=50)
+    description = models.TextField(blank=True)
+    url = models.URLField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
